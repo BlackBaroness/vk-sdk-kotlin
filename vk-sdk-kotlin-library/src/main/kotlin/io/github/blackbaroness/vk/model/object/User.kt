53e4f8f70298cb6830550d4790eae57681b1ed03
BlackBaroness <57033600+BlackBaroness@users.noreply.github.com>
2025-08-14 21:31:31 +0300

    improvements

--- vk-sdk-kotlin-library/src/main/kotlin/io/github/blackbaroness/vk/model/object/User.kt
+++ vk-sdk-kotlin-library/src/main/kotlin/io/github/blackbaroness/vk/model/object/User.kt
@@ -6,6 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class User(
     @SerialName("id") val id: Long,
-    @SerialName("first_name") val firstName: Long,
-    @SerialName("last_name") val lastName: Long,
+    @SerialName("first_name") val firstName: String,
+    @SerialName("last_name") val lastName: String,
 )
